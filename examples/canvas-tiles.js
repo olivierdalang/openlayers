40d9d14361f29613a8b62b60c390498544dc75ac
Iván Sánchez Ortega <ivan@sanchezortega.es>
2019-12-18 07:44:00 +0100

    WIP: working RGBA textures

--- examples/canvas-tiles.js
+++ examples/canvas-tiles.js
@@ -7,16 +7,23 @@ import {OSM, GlTiles} from '../src/ol/source.js';
 
 const map = new Map({
   layers: [
-    new TileLayer({
-      source: new OSM()
-    }),
+//     new TileLayer({
+//       source: new OSM()
+//     }),
     new TileLayer({
       source: new GlTiles({
         fragmentShader: `
         void main(void) {
-          gl_FragColor = vec4(0.2, vTextureCoords,0.5);
+          //gl_FragColor = vec4(0.2, vTextureCoords,0.5);
+
+          vec4 texelColour = texture2D(uTexture0, vec2(vTextureCoords.s, vTextureCoords.t));
+
+          gl_FragColor = vec4(texelColour.gbr, 1.0);
         }
-        `
+        `,
+        textureSources: [
+          new OSM()
+        ]
       })
     })
   ],
